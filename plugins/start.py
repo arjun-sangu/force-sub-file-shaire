b718459d674b96d079cfaaba02c76ddae0fa5503
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-07-31 12:44:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -105,6 +105,7 @@ async def not_joined(client: Client, message: Message):
         text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
     except ValueError:
         pass
+else:
     reply_markup = InlineKeyboardMarkup(
         [     
             [
